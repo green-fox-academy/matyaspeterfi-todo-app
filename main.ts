14706b41544445a9a5f38a6c5ee41711a7eed849
Mat Peterfi <matyas.peterfi@gmail.com>
2019-10-03 12:15:21 +0200

    added -l argument which will allow the user to see the complete itemised tasklist, also added tasklist sourcefile

--- main.ts
+++ main.ts
@@ -5,11 +5,17 @@ const fs = require('fs')
 const args: string[] = process.argv;
 
 let usage = fs.readFileSync("usage.md", "utf-8");
+let taskList = fs.readFileSync("tasklist.md", "utf-8");
 
 let mainProcess = () => {
 
-  if(args[2] == undefined){
+  if(args[2] === undefined){
     console.log(usage);
+  }else if(args[2] === "-l"){
+    let splitTaskList = fs.readFileSync("tasklist.md", "utf-8").split("\n");
+    for (let i = 0; i < splitTaskList.length; i++){
+      console.log(i+1 + " - " + splitTaskList[i]);
+    };
   }
 }
 
