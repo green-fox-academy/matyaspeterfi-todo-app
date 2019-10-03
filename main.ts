ca8858cb6baddbcacfe27b18ce13eb46dd5c6f3d
Mat Peterfi <matyas.peterfi@gmail.com>
2019-10-03 11:54:13 +0200

    added functionality that prints list of usable arguments when no arguments are presented, and added usage source file

--- main.ts
+++ main.ts
@@ -1,5 +1,16 @@
 'use strict'
 
-const fs = require ('fs')
+const fs = require('fs')
 
+const args: string[] = process.argv;
 
+let usage = fs.readFileSync("usage.md", "utf-8");
+
+let mainProcess = () => {
+
+  if(args[2] == undefined){
+    console.log(usage);
+  }
+}
+
+mainProcess();
