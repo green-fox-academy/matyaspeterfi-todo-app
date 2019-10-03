cde0d6f6bd89d76769219e4aef3c44717c16eb7f
Mat Peterfi <matyas.peterfi@gmail.com>
2019-10-03 19:32:34 +0200

    minor formatting changes

--- additem.ts
+++ additem.ts
@@ -4,18 +4,18 @@ const fs = require('fs')
 
 const args: string[] = process.argv;
 
-export class AddItem{
+export class AddItem {
 
-  _destination:string;
+  _destination: string;
 
-  addItem(){
-    if(args[2] === "-a"){
-      if(args[3] != undefined){  
+  addItem() {
+    if (args[2] === "-a") {
+      if (args[3] != undefined) {
         fs.appendFileSync(this._destination, `\n${args[3]}`, "utf-8")
-      }else{console.log("Unable to add: no task provided")}
+      } else { console.log("Unable to add: no task provided") }
     }
   }
-  constructor(destination){
+  constructor(destination) {
     this._destination = destination;
   }
 }
--- noargument.ts
+++ noargument.ts
@@ -11,7 +11,7 @@ export class NoArg {
   public noArgCheck() {
     if (args[2] === undefined) {
       console.log(this._usage);
-    }else if(args[2] !== "-l" && args[2] !== "-r" && args[2] !== "-a" && args[2] !== "-c"){
+    } else if (args[2] !== "-l" && args[2] !== "-r" && args[2] !== "-a" && args[2] !== "-c") {
       console.log("\n---Unsupported argument---\n");
       console.log(this._usage);
     }
